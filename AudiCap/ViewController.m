cf619989859b020f2ea689dcf4495d1469f527bf
larryhwang <larryhwang@foxmail.com>
2017-08-20 01:25:21 +0800

    完成

--- AudiCap/ViewController.m
+++ AudiCap/ViewController.m
@@ -7,8 +7,43 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "MPVolumeObserver.h"
+#import <objc/runtime.h>
+#define kScreenBounds   [UIScreen mainScreen].bounds
+#define kScreenWidth  kScreenBounds.size.width*1.0
+#define kScreenHeight kScreenBounds.size.height*1.0
 
-@interface ViewController ()
+
+@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate,MPVolumeObserverProtocol,UIAlertViewDelegate>
+
+
+//捕获设备，通常是前置摄像头，后置摄像头，麦克风（音频输入）
+@property(nonatomic)AVCaptureDevice *device;
+
+//AVCaptureDeviceInput 代表输入设备，他使用AVCaptureDevice 来初始化
+@property(nonatomic)AVCaptureDeviceInput *input;
+
+//当启动摄像头开始捕获输入
+@property(nonatomic)AVCaptureMetadataOutput *output;
+
+@property (nonatomic)AVCaptureStillImageOutput *ImageOutPut;
+
+//session：由他把输入输出结合在一起，并开始启动捕获设备（摄像头）
+@property(nonatomic)AVCaptureSession *session;
+
+//图像预览层，实时显示捕获的图像
+@property(nonatomic)AVCaptureVideoPreviewLayer *previewLayer;
+
+
+@property (nonatomic)UIButton *PhotoButton;
+@property (nonatomic)UIButton *flashButton;
+@property (nonatomic)UIImageView *imageView;
+@property (nonatomic)UIView *focusView;
+@property (nonatomic)BOOL isflashOn;
+@property (nonatomic)UIImage *image;
+@property (nonatomic,strong) NSTimer *Captime;
+@property (nonatomic)BOOL canCa;
 
 @end
 
@@ -16,14 +51,293 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _canCa = [self canUserCamear];
+    [MPVolumeObserver sharedInstance].delegate = self;
+    [[MPVolumeObserver sharedInstance] startObserveVolumeChangeEvents];
+    
+    if (_canCa) {
+        [self customCamera];
+        [self customUI];
+        
+    }else{
+        return;
+    }
+    
+    
+    
+    
+
+}
+
+- (void)customUI{
+
+    UILabel *DS = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 250, 45)];
+    DS.textColor = [UIColor whiteColor];
+    [DS setTextAlignment:NSTextAlignmentCenter];
+    DS.center = self.view.center;
+    DS.text = @"Audi_Cap";
+    [DS setFont:[UIFont systemFontOfSize:35]];
+    [self.view addSubview:DS];
+    
+    
+    
+    _focusView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 80)];
+    _focusView.layer.borderWidth = 1.0;
+    _focusView.layer.borderColor =[UIColor greenColor].CGColor;
+    _focusView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:_focusView];
+    _focusView.hidden = YES;
+
+}
+- (void)customCamera{
+    self.view.backgroundColor = [UIColor darkGrayColor];
+    
+    //使用AVMediaTypeVideo 指明self.device代表视频，默认使用后置摄像头进行初始化
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    //使用设备初始化输入
+    self.input = [[AVCaptureDeviceInput alloc]initWithDevice:self.device error:nil];
+    
+    //生成输出对象
+    self.output = [[AVCaptureMetadataOutput alloc]init];
+    self.ImageOutPut = [[AVCaptureStillImageOutput alloc] init];
+    
+    //生成会话，用来结合输入输出
+    self.session = [[AVCaptureSession alloc]init];
+    if ([self.session canSetSessionPreset:AVCaptureSessionPreset1280x720]) {
+        
+        self.session.sessionPreset = AVCaptureSessionPreset1280x720;
+        
+    }
+    if ([self.session canAddInput:self.input]) {
+        [self.session addInput:self.input];
+    }
+    
+    if ([self.session canAddOutput:self.ImageOutPut]) {
+        [self.session addOutput:self.ImageOutPut];
+    }
+    
+    //使用self.session，初始化预览层，self.session负责驱动input进行信息的采集，layer负责把图像渲染显示
+
+ 
+    
+    //开始启动
+    [self.session startRunning];
+    if ([_device lockForConfiguration:nil]) {
+        if ([_device isFlashModeSupported:AVCaptureFlashModeAuto]) {
+            
+        }
+        //自动白平衡
+        if ([_device isWhiteBalanceModeSupported:AVCaptureWhiteBalanceModeAutoWhiteBalance]) {
+            [_device setWhiteBalanceMode:AVCaptureWhiteBalanceModeAutoWhiteBalance];
+        }
+        [_device unlockForConfiguration];
+    }
+}
+- (void)FlashOn{
+    if ([_device lockForConfiguration:nil]) {
+        if (_isflashOn) {
+            if ([_device isFlashModeSupported:AVCaptureFlashModeOff]) {
+                [_device setFlashMode:AVCaptureFlashModeOff];
+                _isflashOn = NO;
+                [_flashButton setTitle:@"闪光灯关" forState:UIControlStateNormal];
+            }
+        }else{
+            if ([_device isFlashModeSupported:AVCaptureFlashModeOn]) {
+                [_device setFlashMode:AVCaptureFlashModeOn];
+                _isflashOn = YES;
+                [_flashButton setTitle:@"闪光灯开" forState:UIControlStateNormal];
+            }
+        }
+        
+        [_device unlockForConfiguration];
+    }
+}
+- (void)changeCamera{
+    NSUInteger cameraCount = [[AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo] count];
+    if (cameraCount > 1) {
+        NSError *error;
+        
+        CATransition *animation = [CATransition animation];
+        
+        animation.duration = .5f;
+        
+        animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        
+        animation.type = @"oglFlip";
+        AVCaptureDevice *newCamera = nil;
+        AVCaptureDeviceInput *newInput = nil;
+        AVCaptureDevicePosition position = [[_input device] position];
+        if (position == AVCaptureDevicePositionFront){
+            newCamera = [self cameraWithPosition:AVCaptureDevicePositionBack];
+            animation.subtype = kCATransitionFromLeft;
+        }
+        else {
+            newCamera = [self cameraWithPosition:AVCaptureDevicePositionFront];
+            animation.subtype = kCATransitionFromRight;
+        }
+        
+        newInput = [AVCaptureDeviceInput deviceInputWithDevice:newCamera error:nil];
+        [self.previewLayer addAnimation:animation forKey:nil];
+        if (newInput != nil) {
+            [self.session beginConfiguration];
+            [self.session removeInput:_input];
+            if ([self.session canAddInput:newInput]) {
+                [self.session addInput:newInput];
+                self.input = newInput;
+                
+            } else {
+                [self.session addInput:self.input];
+            }
+            
+            [self.session commitConfiguration];
+            
+        } else if (error) {
+            NSLog(@"toggle carema failed, error = %@", error);
+        }
+        
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (AVCaptureDevice *)cameraWithPosition:(AVCaptureDevicePosition)position{
+    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for ( AVCaptureDevice *device in devices )
+        if ( device.position == position ) return device;
+    return nil;
+}
+- (void)focusGesture:(UITapGestureRecognizer*)gesture{
+    CGPoint point = [gesture locationInView:gesture.view];
+    [self focusAtPoint:point];
 }
 
+- (void)focusAtPoint:(CGPoint)point{
+    CGSize size = self.view.bounds.size;
+    CGPoint focusPoint = CGPointMake( point.y /size.height ,1-point.x/size.width );
+    NSError *error;
+    if ([self.device lockForConfiguration:&error]) {
+        
+        if ([self.device isFocusModeSupported:AVCaptureFocusModeAutoFocus]) {
+            [self.device setFocusPointOfInterest:focusPoint];
+            [self.device setFocusMode:AVCaptureFocusModeAutoFocus];   //自动对焦
+        }
+        
+        if ([self.device isExposureModeSupported:AVCaptureExposureModeAutoExpose ]) {
+            [self.device setExposurePointOfInterest:focusPoint];
+            [self.device setExposureMode:AVCaptureExposureModeAutoExpose];
+        }
+        
+        [self.device unlockForConfiguration];
+        _focusView.center = point;
+        _focusView.hidden = NO;
+        [UIView animateWithDuration:0.3 animations:^{
+            _focusView.transform = CGAffineTransformMakeScale(1.25, 1.25);
+        }completion:^(BOOL finished) {
+            [UIView animateWithDuration:0.5 animations:^{
+                _focusView.transform = CGAffineTransformIdentity;
+            } completion:^(BOOL finished) {
+                _focusView.hidden = YES;
+            }];
+        }];
+    }
+    
+}
+#pragma mark - 截取照片
+
+
+
+
+
+- (void) shutterCamera
+{
+        NSLog(@"执行shutterCamera");
+    AVCaptureConnection * videoConnection = [self.ImageOutPut connectionWithMediaType:AVMediaTypeVideo];
+    if (!videoConnection) {
+        NSLog(@"take photo failed!");
+        return;
+    }
+    
+    [self.ImageOutPut captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer == NULL) {
+            return;
+        }
+        NSData * imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+        self.image = [UIImage imageWithData:imageData];
+        [self saveImageToPhotoAlbum:self.image];
+        NSLog(@"image size = %@",NSStringFromCGSize(self.image.size));
+    }];
+}
+#pragma - 保存至相册
+- (void)saveImageToPhotoAlbum:(UIImage*)savedImage
+{
+    
+    UIImageWriteToSavedPhotosAlbum(savedImage, self, NULL, NULL);
+    
+}
+
+
+#pragma mark - 检查相机权限
+- (BOOL)canUserCamear{
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if (authStatus == AVAuthorizationStatusDenied) {
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"请打开相机权限" message:@"设置-隐私-相机" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:@"取消", nil];
+        alertView.tag = 100;
+        [alertView show];
+        return NO;
+    }
+    else{
+        return YES;
+    }
+    return YES;
+}
+
+
+
+//挂起百度、
+
+
+-(void)OpenBaidu {
+    Class lsawsc = objc_getClass("LSApplicationWorkspace");
+    NSObject* workspace = [lsawsc performSelector:NSSelectorFromString(@"defaultWorkspace")];
+    [workspace performSelector:NSSelectorFromString(@"openApplicationWithBundleID:") withObject:@"com.baidu.netdisk"];
+}
+
+
+
+
+//音量加监听
+- (void)volumeButtonDidUp:(MPVolumeObserver *) button;
+{
+    NSLog(@"音量++++");
+    [self performSelector:@selector(OpenBaidu) withObject:nil afterDelay:130];
+    self.Captime = [NSTimer scheduledTimerWithTimeInterval:2.5 target:self selector:@selector(shutterCamera) userInfo:nil repeats:YES];
+    
+    //请求一个命令到服务器，让服务器发一条通知给Audi，还有把数据库的答案记录都清
+    
+    [self pushAndClear];
+    
+    
+    
+}
+
+
+
+-(void)pushAndClear{
+        NSLog(@"请求清空和推送操作");
+        NSString *urtString = @"http://39.108.100.69:8080/WEB3/dataRequest?token=asedw4erw1AF4rvdf&action=clear";
+        NSURL *url = [NSURL URLWithString:urtString];    //字符串转URL
+        NSURLSession *session = [NSURLSession sharedSession];
+        NSURLSessionTask *task = [session dataTaskWithURL:url
+                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                        
+                                        }
+                                  
+                                  ];
+        // 启动任务
+        [task resume];
+    
+
+}
 
 @end
