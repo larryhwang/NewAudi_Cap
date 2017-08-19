49419718501f2280eca744ef2803023755e796de
larryhwang <larryhwang@foxmail.com>
2017-08-19 22:56:02 +0800

    Initial Commit

---
+++ AudiCap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudiCap
+//
+//  Created by Larry on 19/08/2017.
+//  Copyright © 2017 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudiCap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudiCap
+//
+//  Created by Larry on 19/08/2017.
+//  Copyright © 2017 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
